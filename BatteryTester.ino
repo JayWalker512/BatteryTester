72c6edbd962a7e51d7de91459d645f2797d412fe
Brandon Foltz <bsfoltzguitar@gmail.com>
2018-09-01 21:49:43 -0400

    Test starting works, now just need to add safety for testing LiIons

--- BatteryTester.ino
+++ BatteryTester.ino
@@ -1,11 +1,15 @@
 #define CONTROL_PIN 5
 #define SENSE_PIN A5
-#define DEFAULT_CURRENT_LIMIT 100
+#define DEFAULT_CURRENT_LIMIT 0.1f
+
+bool testStarted = false;
+long elapsedTestTime = 0;
+float ampSeconds = 0;
 
 /* This assumes the sense pin is measuring the voltage
  * across a 1 Ohm shunt resistor.
  */
-float convertSenseToMa(int sensedValue) {
+float convertSenseToA(int sensedValue) {
   //10-bit ADC, 5 volt reference
   return ((float)sensedValue / 1024.0f) * 5.0f; 
   //return (float)sensedValue;
@@ -34,21 +38,50 @@ void loop() {
   analogWrite(CONTROL_PIN, currentControlValue); 
 
   //adjust the value up until we get to DEFAULT_CURRENT_LIMIT
-  float currentCurrent = convertSenseToMa(avgAnalogRead(SENSE_PIN));
-  //float currentCurrent = (float)avgAnalogRead(SENSE_PIN);
-  if (currentCurrent < 0.1f) {
+  float currentCurrent = convertSenseToA(avgAnalogRead(SENSE_PIN));
+  if (currentCurrent < DEFAULT_CURRENT_LIMIT) {
     currentControlValue = (currentControlValue + 1) % 255;
   }
 
-  if (currentCurrent > 0.1f) {
+  if (currentCurrent > DEFAULT_CURRENT_LIMIT) {
     currentControlValue = (currentControlValue - 1) % 255;
   }
 
-  Serial.print("Current: ");
-  Serial.print(currentCurrent, DEC);
-  Serial.print(" Control: ");
-  Serial.print(currentControlValue, DEC);
-  Serial.println("");
+
+
+  static long lastSampleTimestamp = 0;
+  if (currentCurrent > DEFAULT_CURRENT_LIMIT && testStarted == false) {
+    testStarted = true;
+    elapsedTestTime = 0;
+    lastSampleTimestamp = millis();
+    ampSeconds = 0;
+    Serial.println("Test started...");
+  }
+
+  long currentTimestamp = millis();
+  if (currentTimestamp > lastSampleTimestamp + 1000) {
+    long elapsedSampleTime = currentTimestamp - lastSampleTimestamp;
+    elapsedTestTime += elapsedSampleTime;
+
+    float sampleDuration = elapsedSampleTime / 1000.0f;
+    ampSeconds += (sampleDuration * currentCurrent);
+
+    lastSampleTimestamp = currentTimestamp;
+
+    if (testStarted) {
+      Serial.print("Amp Seconds consumed: ");
+      Serial.print(ampSeconds, DEC);
+      Serial.println("");
+    } else {
+      Serial.print("Current: ");
+      Serial.print(currentCurrent, DEC);
+      Serial.print(" Control: ");
+      Serial.print(currentControlValue, DEC);
+      Serial.println("");
+    }
+  }
+
+  
   delay(100);
 }
 
